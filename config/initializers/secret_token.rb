3d655b8c93418afbc3e26ea5dc3f3b8a53045dcf
Keith Zimmerman <keithrz@gmail.com>
2014-02-01 18:35:24 -0600

    Initial project creation.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsPlanMyHike::Application.config.secret_key_base = '3b0d2c1d632d0c64c103ebb1d0eac8466a47a4fb24f54c58baa2d691e44c77370ea78da94e6e6524e6903ff110b11fa3dbc5f3c6c16ebebf8e3478e35938165e'
